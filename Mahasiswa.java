4e8f23ba78d7897a3babb44dd358382cf0ce5dec
hayatun syauli <syauli1601@gmail.com>
2021-06-01 16:26:36 +0700

    Initial sharing of project

---
+++ Mahasiswa.java
@@ -0,0 +1,61 @@
+
+/**
+ * Write a description of class Mahasiswa here.
+ *
+ * @author (Hayatun Syauli)
+ * @version (a version number or a date)
+ */
+public class Mahasiswa
+{
+    // instance variables - replace the example below with your own
+    private int nim, umur;
+    private String nama, alamat, kelas;
+    
+    
+    /**
+     * Constructor for objects of class Mahasiswa
+     */
+    public Mahasiswa(int nim, int umur, String nama, String alamat, String kelas)
+    {
+        this.nim = nim;
+        this.umur = umur;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.kelas = kelas;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String getnamaMhs()
+    {
+        return this.nama;
+    }
+    
+    public String getalamatMhs()
+    {
+        return this.alamat;
+    }
+    
+    public String getkelasMhs()
+    {
+        return this.kelas;
+    }
+    
+    public int getnimMhs()
+    {
+        return this.nim;
+    }
+    
+    public int getumurMhs()
+    {
+        return this.umur;
+    }
+    
+    public void setnamaMhs(String nama){
+        this.nama = nama;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,134 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+/**
+ * Write a description of class Main here.
+ *
+ * @author (Hayatun syauli)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    public static void main(String args[]){
+        
+        //ArrayList<Integer> arri = new ArrayList<Integer>();
+        //ArrayList<String> arrs = new ArrayList<String>();
+        ArrayList<Mahasiswa> arrm = new ArrayList<Mahasiswa>();
+        
+        System.out.println("Data Mahasiswa");
+        System.out.print("Jumlah Mahasiswa: ");
+        Scanner input = new Scanner(System.in);
+        int jumlah=0, nim=0,umur=0;
+        String nama=null,alamat=null,kelas=null;
+        jumlah = input.nextInt();
+    
+        //Menginput Data Mahasiswa
+        for(int i=0; i < jumlah; i++){
+            
+            System.out.print("Nim ke-"+(i+1)+": ");
+            nim = input.nextInt();
+            System.out.print("Nama ke-"+(i+1)+": ");
+            nama = input.next();
+            System.out.print("Alamat "+(i+1)+": ");
+            alamat = input.next();
+            System.out.print("Kelas "+(i+1)+": ");
+            kelas = input.next();
+            System.out.print("Umur "+(i+1)+": ");
+            umur = input.nextInt();
+            arrm.add(new Mahasiswa(nim,umur,nama,alamat,kelas));
+            System.out.println("\n");
+            
+        }
+        
+        System.out.println("\n");
+        System.out.println("Menampilkan Data Mahasiswa");
+        for (Mahasiswa mhs : arrm)
+        {
+            System.out.println("Nim : "+mhs.getnimMhs());
+            System.out.println("Nama : "+mhs.getnamaMhs());
+            System.out.println("Alamat : "+mhs.getalamatMhs());
+            System.out.println("Kelas : "+mhs.getkelasMhs());
+            System.out.println("Umur : "+mhs.getumurMhs());
+            System.out.println("---------------------");
+        }
+        
+        //Mencari data Mahasiswa
+        System.out.println("\nSearch Data Mahasiswa");
+        int cariNim;
+        System.out.print("Cari NIM: ");
+        cariNim = input.nextInt();
+        System.out.println("Hasil yang dicari:");
+        for (Mahasiswa mhs : arrm)
+        {
+            if(mhs.getnimMhs() == cariNim){
+                System.out.println("Nim : "+mhs.getnimMhs());
+                System.out.println("Nama : "+mhs.getnamaMhs());
+                System.out.println("Alamat : "+mhs.getalamatMhs());
+                System.out.println("Kelas : "+mhs.getkelasMhs());
+                System.out.println("Umur : "+mhs.getumurMhs());
+            }
+        }
+        /*
+        System.out.println("Menampilkan Data Mahasiswa");
+        for (Mahasiswa mhs: arrm)
+        {
+            if(mhs.getnimMhs() == 1001)
+            {
+                arrm.indexOf(mhs);
+                arrm.remove(arrm.indexOf(mhs));
+                break;
+            }
+            System.out.println(mhs.getnimMhs()+" - "+ mhs.getnamaMhs());
+        }
+        */
+       
+       //-----------------------------
+       /*
+        System.out.println("----------------------------------------------------");
+        System.out.println("----------------------------------------------------");
+        System.out.println("Menampilkan Nilai Array List dengan for biasa");
+        for(int i=0; i < arri.size(); i++){
+            System.out.println(arri.get(i));
+            //System.out.println(arrs.get(i));
+        }
+        
+        System.out.println("");
+        Collections.sort(arri);
+        System.out.println("Menampilkan Nilai Array List iterator");
+        for (Integer valx: arri){
+            System.out.println(valx);
+        }
+        
+        System.out.println("");
+        System.out.println("Menampilkan Nilai Array sebelum sort");
+        for (String valx: arrs){
+            System.out.println(valx);
+        }
+        
+        System.out.println("");
+        Collections.sort(arrs);
+        System.out.println("Menampilkan Nilai Array setelah sort");
+        for (String valx: arrs){
+            System.out.println(valx);
+        }
+      
+          */
+        
+        /*
+        arri.add(1);
+        arri.add(2);
+        arri.add(5);
+        arri.add(4);
+        
+        arrs.add("Doni");
+        arrs.add("Ayu");
+        arrs.add("Bayu");
+        arrs.add("Syauli");
+        
+        //arrm.add(new Mahasiswa(1001,"Doni"));
+        
+        */
+    }
+    
+}
